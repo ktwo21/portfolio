22ebe1893cbf1fd960c0e80e815041142317552f
du_papa <k2merci@gmail.com>
2021-04-22 23:51:15 +0900

    4.22　微調整

--- JS/style.js
+++ JS/style.js
@@ -1,44 +1,31 @@
-//ハンバーガーメニュー
-
-const jsHamburger = document.getElementById('js-hamburger');
-//buttonタグ（ハンバーガーボタン）を定数として定義
-
-const body = document.body;
-//body全体を定数として定義
-
-const spHeaderMenu = document.getElementById('js-global-menu');
-//ハンバーガーメニューを定数として定義
-
-jsHamburger.addEventListener('click',function(){
-  //ハンバーガーボタンをクリックした時に
-  body.classList.toggle('is-drawerActive')
-  //bodyに is-drawerActive というクラスを付与する（divツールで見たときに動かしているのかわかりやすくするために付与？）
-  if(this.getAttribute('aria-expanded')== 'false'){
-    //もし、buttonタグが aria-expanded=false（抱擁する要素（メニュー）が縮んでいるよ）という要素を持っていたら、＝メニューが閉じていたら
-    this.setAttribute('aria-expanded','true');
-    //buttonタグに aria-expanded=true（メニューが開いているよ）という要素を付与します➤CSSに影響
-    spHeaderMenu.style.visibility ="visible";
-    //ハンバーガーメニューにvisibility（視認性）というCSSを付与し、値をvisible（見える）にする
-    spHeaderMenu.setAttribute('area-hidden','false')
-    //ハンバーガーメニューにarea-hidden=false（要素を隠さない）を付与する
-  }else{
-    //もし、buttonタグがaria-expanded=false（抱擁する要素が父んでいるよ）という要素をもっていなかった場合＝メニューが開いていたら
-    this.setAttribute('aria-expanded','false');
-    //buttonタグにメニューが閉じているよという要素を付与して、➤CSSに影響
-    spHeaderMenu.style.visibility ="hidden";
-    //ハンバーガーメニューをかくして
-    spHeaderMenu.setAttribute('area-hidden','true')
-    //ハンバーガーメニューに隠れてますという要素を付与する
-  };
-});
 
 //ふわっと表示
 
 AOS.init();
 
-//Google form
+
 $(document).ready(function () {
 
+  // //ハンバーガーメニュー
+  $('#js-hamburger').on('click',function(){
+    $('body').toggleClass('active')
+    if($('#js-hamburger').attr('aria-expanded')=='false'){
+      $(this).attr('aria-expanded','true');
+      $('.sp-global-menu').attr('area-hidden','false').css('visibility','visible');
+    }else{
+      $(this).attr('aria-expanded','false');
+      $('.sp-global-menu').attr('area-hidden','true').css('visibility','hidden');
+    }
+  })
+  $('.hamburger-list').on('click',function(){
+    $('body').toggleClass('active')
+      $('#js-hamburger').attr('aria-expanded','false');
+      $('.sp-global-menu').attr('area-hidden','true').css('visibility','hidden');
+    
+  })
+
+
+  //Google form
   $('#form').submit(function (event) {
     var formData = $('#form').serialize();
     $.ajax({
